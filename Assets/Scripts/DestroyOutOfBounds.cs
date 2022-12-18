9dd946256e3c268ed15e8f992f3649cd42b134de
B00140738 <B00140738@mytudublin.ie>
2022-12-18 18:01:11 +0000

    update

---
+++ Assets/Scripts/DestroyOutOfBounds.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyOutOfBounds : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+	private float topBound = 80;
+	private float lowerBound = -250;
+    // Update is called once per frame
+    void Update(){
+	
+   		if(transform.position.z > topBound){
+			Destroy(gameObject);
+		}
+		else if(transform.position.z < lowerBound){
+			Debug.Log("Game over!");
+			Destroy(gameObject);
+		}	
+	}
+}
+
